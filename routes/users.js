d7fcdd6ed37d494ef95df3126ec32446f8270905
IsoHovhannisyan <143291832+IsoHovhannisyan@users.noreply.github.com>
2024-01-08 05:25:25 +0400

    changed mongodb

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const { add, edit, remove, all, User } = require("../controllers/users");
 router.get('/', all);
 router.get('/:id', User);
 router.post('/add', add );
-router.post("/remove/:id", remove);
+router.post("/remove/:id", remove); 
 router.put("/edit/:id", edit); 
 
 
